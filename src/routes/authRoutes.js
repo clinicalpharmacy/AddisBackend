06f025a601b3f2c208f5f31248a0e1f1597f6e88
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-03-01 14:55:59 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -223,8 +223,6 @@ router.post('/login', async (req, res) => {
             tin_number: user.tin_number || '',
             license_number: user.license_number || '',
             email_verified: user.email_verified,
-            is_healthcare_client: isHealthcareClient,
-            healthcare_client_id: isHealthcareClient ? user.id : null
         };
 
         res.json({
@@ -530,11 +528,6 @@ router.get('/me', authenticateToken, async (req, res) => {
             }
         }
 
-        const isEffectivelyCompanyUser = user.user_type === 'company_user' || !!user.company_id;
-        const isHealthcareClient = !isEffectivelyCompanyUser && 
-            (user.role === 'healthcare_client' || 
-             user.account_type === 'individual' || 
-             user.user_type === 'individual');
 
         const userResponse = {
             id: user.id,
